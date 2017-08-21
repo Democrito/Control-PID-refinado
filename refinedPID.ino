5ccbf050b39f17ddebc57e895d721b0535b1f851
Democrito <epicureo_a@hotmail.com>
2017-08-21 11:12:08 +0200

    Update refinedPID.ino

--- refinedPID.ino
+++ refinedPID.ino
@@ -22,6 +22,74 @@ unsigned int  tmp      =  0;             // Variable que utilizaremos para poner
 const byte    ledok    = 13;             // El pin 13 de los Arduinos tienen un led que utilizo para mostrar que el motor ya ha llegado a la posición designada.
 // ****************************************************************************************************************************************************************
 
+void encoder()                        // Encoder x4. Cuando se produzca cualquier cambio en el encoder esta parte hará que incremente o decremente el contador.
+{
+    ant=act;                          // Guardamos el valor 'act' en 'ant' para convertirlo en pasado.
+    act=PIND & 12;                    // Guardamos en 'act' el valor que hay en ese instante en el encoder y hacemos un
+                                      // enmascaramiento para aislar los dos únicos bits que utilizamos para esta finalidad.
+    if(ant==12 && act==4)  contador++;// Incrementa el contador si el encoder se mueve hacia delante.
+    if(ant==4  && act==0)  contador++;
+    if(ant==0  && act==8)  contador++;
+    if(ant==8  && act==12) contador++;
+    
+    if(ant==4  && act==12) contador--;// Decrementa el contador si el encoder se mueve hacia atrás.
+    if(ant==0  && act==4)  contador--;
+    if(ant==8  && act==0)  contador--;
+    if(ant==12 && act==8)  contador--;
+}
+
+double Compute(void)
+{
+   unsigned long now = millis();                  // Toma el número total de "ticks" que hay en ese instante. (1 tick = 1 milisengundo.)
+   unsigned long timeChange = (now - lastTime);   // Resta el tiempo actual con el último tiempo que se guardó (esto último se hace al final de esta función).
+   
+   if(timeChange >= SampleTime)                   // Si se cumple el tiempo de muestreo entonces calcula la salida.
+   {
+     double kn=kp;                                // Carga el valor KP (constante proporcional) en KN para poder modificarlo sin modificar el valor original.
+     
+     Input  = (double)contador;                   // Lectura del encoder óptico. El valor del contador se incrementa/decrementa a través de las interrupciones extrenas (pines 2 y 3).
+     error  = (Setpoint - Input)  * kn;           // Calcula el error proporcional.
+     dInput = (Input - lastInput) * kd;           // Calcula el error derivativo.
+     
+     if (abs(error) < 0.1) kn = kp * 100;         // Cuando el error es muy pequeño (cuando está prácticamente en la posición designada) exagera el valor KP para "clavar" el motor en ese punto.     
+     
+     if (dInput == 0.0) ITerm += (error * ki); else ITerm -= (dInput / (kn * kd)); // Esta línea permite dos cosas. 1) Suaviza la llegada a la posición designada. 2) Hace que el error integral no sea tan dependiente del error proporcional, sólo actúa cuando está muy cerca de la posición designada.
+     if (ITerm > outMax) ITerm = outMax; else if (ITerm < outMin) ITerm = outMin;  // Delimita el error integral para eliminar el "efecto windup".
+     
+     double out = error + ITerm - dInput;          // Suma todos los errores, es la salida del control PID.
+     if (out > outMax) out = outMax; else if (out < outMin) out = outMin; // Delimita la salida para que pueda estar el PWM entre un valor de 0 y 5 voltios.
+     
+     lastInput = Input;                            // Se guarda la posición para convertirla en pasado.
+     lastTime  = now;                              // Se guarda el tiempo   para convertirlo en pasado.
+     return out;                                   // Devuelve el valor de salida PID. Este 'out' es diferente del 'Out' del programa principal, sin embargo éste se transfiere al otro.
+   }
+}
+
+void SetTunings(double kp, double ki, double kd)   // A las constantes KI y KD se le incluye el tiempo de muestreo. Así sólo se ha de hacer esto una sola vez.
+{
+   double SampleTimeInSec = ((double)SampleTime) / 1000;
+   kp = kp;
+   ki = ki * SampleTimeInSec;
+   kd = kd / SampleTimeInSec;
+}
+
+void SetSampleTime(int NewSampleTime)              // Función que se llama cuando queremos poner un tiempo de muestreo dado.
+{                                                  // Evita que el tiempo de muestreo sea 0 o negativo.
+   if (NewSampleTime > 0) SampleTime = (unsigned long)NewSampleTime;
+}
+
+void imprimir(byte flag) // Si "flag" es 0, imprime en el terminal serie los datos de las contantes PID, tiempo de muestreo y posición. En los demás casos sólo imprime la posición del motor.
+{
+  if (flag==0)
+  {
+    Serial.print("KP=");     Serial.print(kp);
+    Serial.print(" KI=");    Serial.print(ki);
+    Serial.print(" KD=");    Serial.print(kd);
+    Serial.print(" Time=");  Serial.println(SampleTime);
+  }
+  Serial.print("Posicion:"); Serial.println((long) Setpoint);
+}
+
 void setup()                          // Inicializamos todo las variables que sean necesarias, configuramos los pines de entrada/salida y configuramos el terminal serie.
 {
   Serial.begin(115200);               // Configura la velocidad en baudios del terminal serie. Hoy en día "115200" es soportada por la gran mayoría de computadores.
@@ -73,14 +141,14 @@ void loop()
       digitalWrite(PWMB, 0);          // Pone a 0 el segundo pin del puente en H.
       analogWrite(PWMA, abs(Out));    // Por el primer pin sale la señal PWM.
     }
-    if (Out < 0.0)                    // Mueve el motor hacia  atrás   con el PWM correspondiente a su posición.
+    else                              // Mueve el motor hacia  atrás   con el PWM correspondiente a su posición.
     {
       digitalWrite(PWMA, 0);          // Pone a 0 el primer pin del puente en H.
       analogWrite(PWMB, abs(Out));    // Por el segundo pin sale la señal PWM.
     }
   }
   
-  // Recepción de datos para mover el motor o modificar las constantes PID o el tiempo de muestreo.
+// Recepción de datos para mover el motor o modificar las constantes PID o el tiempo de muestreo.
   if (Serial.available() > 0)         // Comprueba si ha recibido algún dato por el terminal serie.
   {
     cmd = Serial.read();              // "cmd" guarda el byte recibido.
@@ -100,81 +168,10 @@ void loop()
       case 'I': ki  = Serial.parseFloat(); SetTunings(kp, ki, kd); imprimir(0); break;
       case 'D': kd  = Serial.parseFloat(); SetTunings(kp, ki, kd); imprimir(0); break;
       case 'T': tmp = Serial.parseInt();   SetSampleTime(tmp);     imprimir(0); break;
-      case 'M': Setpoint = Serial.parseFloat();                    imprimir(1); break; // Esta línea permite introducir una posición absoluta. Ex: m13360 (y luego enter) e irá a esa posición absoluta.
+      case 'G': Setpoint = Serial.parseFloat();                    imprimir(1); break; // Esta línea permite introducir una posición absoluta. Ex: g13360 (y luego enter) e irá a esa posición absoluta.
       case 'K': imprimir(0); break; 
       default:  imprimir(1); break;
     }
     digitalWrite(ledok, LOW);         // Cuando entra una nueva posición se apaga el led y no se volverá a encender hasta que el motor llegue a la posición que le hayamos designado.
   }
-}
-
-double Compute(void)
-{
-   unsigned long now = millis();                  // Toma el número total de "ticks" que hay en ese instante. (1 tick = 1 milisengundo.)
-   unsigned long timeChange = (now - lastTime);   // Resta el tiempo actual con el último tiempo que se guardó (esto último se hace al final de esta función).
-   
-   if(timeChange >= SampleTime)                   // Si se cumple el tiempo de muestreo entonces calcula la salida.
-   {
-     Input  = (double)contador;                   // Lectura del encoder óptico. El valor del contador se incrementa/decrementa a través de las interrupciones extrenas (pines 2 y 3).
-     
-     error  = (Setpoint - Input)  * kp;           // Calcula el error proporcional.
-          
-     dInput = (Input - lastInput) * kd;           // Calcula el error derivativo.
-
-     double kn = kp;                              // Hago que KP se carge en otra variable (KN) de esta manera puedo manipular ese valor sin afectar al valor original.
-     
-     if (error==0.0) kn *= 100.0;                 // Esto nos sirve para "empequeñecer" ITerm en la línea de abajo. (... else ITerm -= dInput / (kn * kd); ) Ayuda a "clavar" el motor en el lugar designado y elimina parte del "sonido" mientras intenta "clavarse".
-     
-     if (dInput == 0.0) ITerm += error * ki; else ITerm -= dInput / (kn * kd);       // Esta línea permite dos cosas. 1) Suaviza la llegada a la posición designada. 2) Hace que el error integral no sea tan dependiente del error proporcional, se consigue que el error integral sólo actúe cuando está muy muy cerca de la posición designada.
-                                                                                     // Para una mayor suavidad al acercarse al punto designado puedes cambiar el "else" por este otro: else ITerm -= (dInput / (kn * ki * kd));
-     if (ITerm > outMax) ITerm = outMax; else if (ITerm < outMin) ITerm = outMin;    // Delimita el error integral para eliminar el "efecto windup".
-     
-     double out = error + ITerm - dInput;          // Suma todos los errores, es la salida del control PID.
-     if (out > outMax) out = outMax; else if (out < outMin) out = outMin; // Delimita la salida para que pueda estar el PWM entre un valor de 0 y 5 voltios.
-     
-     lastInput = Input;                            // Se guarda la posición actual para convertirla en pasado.
-     lastTime  = now;                              // Se guarda el tiempo   actual para convertirlo en pasado.
-     return out;                                   // Devuelve el valor de salida PID. Este 'out' es diferente del 'Out' del programa principal, sin embargo éste se transfiere al otro.
-   }
-}
-
-void SetTunings(double kp, double ki, double kd)   // A las constantes KI y KD se le incluye el tiempo de muestreo. Así sólo se ha de calcular esto una sola vez.
-{
-   double SampleTimeInSec = ((double)SampleTime) / 1000.0;
-   kp = kp;
-   ki = ki * SampleTimeInSec;
-   kd = kd / SampleTimeInSec;
-}
-
-void SetSampleTime(int NewSampleTime)              // Función que se llama cuando queremos poner un tiempo de muestreo dado.
-{                                                  // Evita que el tiempo de muestreo sea 0 o negativo.
-   if (NewSampleTime > 0) SampleTime = (unsigned long)NewSampleTime;
-}
-
-void imprimir(byte flag) // Si "flag" es 0, imprime en el terminal serie los datos de las contantes PID, tiempo de muestreo y posición. En los demás casos sólo imprime la posición del motor.
-{
-  if (flag==0)
-  {
-    Serial.print("KP=");     Serial.print(kp);
-    Serial.print(" KI=");    Serial.print(ki);
-    Serial.print(" KD=");    Serial.print(kd);
-    Serial.print(" Time=");  Serial.println(SampleTime);
-  }
-  Serial.print("Posicion:"); Serial.println((long) Setpoint);
-}
-
-void encoder(void)                    // Encoder x4. Cuando se produzca cualquier cambio en el encoder esta parte hará que incremente o decremente el contador.
-{
-    ant=act;                          // Guardamos el valor 'act' en 'ant' para convertirlo en pasado.
-    act=PIND & 12;                    // Guardamos en 'act' el valor que hay en ese instante en el encoder y hacemos un
-                                      // enmascaramiento para aislar los dos únicos bits que utilizamos para esta finalidad.
-    if(ant==12 && act==4)  contador++;// Incrementa el contador si el encoder se mueve hacia delante.
-    if(ant==4  && act==0)  contador++;
-    if(ant==0  && act==8)  contador++;
-    if(ant==8  && act==12) contador++;
-    
-    if(ant==4  && act==12) contador--;// Decrementa el contador si el encoder se mueve hacia atrás.
-    if(ant==0  && act==4)  contador--;
-    if(ant==8  && act==0)  contador--;
-    if(ant==12 && act==8)  contador--;
 }
