8bda784b72d6faf392a7fd3c3b6d8fed54fcf456
Democrito <epicureo_a@hotmail.com>
2017-08-23 10:49:22 +0200

    Update.
    Segunda versión.

--- refinedPID.ino
+++ refinedPID.ino
@@ -1,4 +1,7 @@
 // Este programa está basado en el Control PID de Brett Beauregard: http://brettbeauregard.com/blog/wp-content/uploads/2012/07/Gu%C3%ADa-de-uso-PID-para-Arduino.pdf
+// Está enfocado para el control de un motor y el algoritmo original lo modifiqué para "suavizar" la respuesta de dicho motor dándole unas características únicas, entre ellas una llegada
+// al punto designado mucho más suave y progresivo y permite "sintonizar" las constantes PID de un modo más intuitivo y sencillo. Más información:
+// https://sites.google.com/site/proyectosroboticos/control-de-motores/control-pid-mejorado (A día de hoy la página está en construcción.)
 
 // **********************************************  Patillaje ******************************************************************************************************
 const byte    encA = 2;                  // Entrada de la señal A del encoder.
@@ -12,13 +15,13 @@ double        Input    = 0.0, Setpoint   = 0.0;                  //     "     de
 double        ITerm    = 0.0, dInput     = 0.0, lastInput = 0.0; //     "     de error integral, error derivativo y posición anterior del motor
 double        kp       = 0.0, ki         = 0.0, kd        = 0.0; // Constantes: proprocional, integral y derivativa.
 double        outMin   = 0.0, outMax     = 0.0;                  // Límites para no sobrepasar la resolución del PWM.
-double        error    = 0.0, kpid       = 0.0;                  // Diferencia entre la posición real del motor y la posición designada.
+double        error    = 0.0;                                    // Diferencia entre la posición real del motor y la posición designada.
 
 // **************************************************** Otras Variables *******************************************************************************************
 volatile long contador =  0;             // En esta variable se guardará los pulsos del encoder y que interpreremos como distancia (o ángulo si ese fuese el caso).
 byte          ant      =  0,    act = 0; // Sólo se utiliza los dos primeros bits de estas variables y servirán para decodificar el encoder. (ant=anterior, act=actual.)
 byte          cmd      =  0;             // Un byte que utilizamos para la comunicación serie. (cmd=comando.)
-unsigned int  tmp      =  0;             // Variable que utilizaremos para poner el tiempo de muestreo.
+unsigned int  tmp      =  0;             // Variable que utilizaremos para asignar el tiempo de muestreo.
 const byte    ledok    = 13;             // El pin 13 de los Arduinos tienen un led que utilizo para mostrar que el motor ya ha llegado a la posición designada.
 // ****************************************************************************************************************************************************************
 
@@ -28,31 +31,31 @@ void setup()                          // Inicializamos todo las variables que se
   
   pinMode(PWMA, OUTPUT);              // Declara las dos salidas PWM para el control del motor (pin 5).
   pinMode(PWMB, OUTPUT);              //           "               "               "           (pin 6).
-  digitalWrite(PWMA, LOW);            // Y ambas salidas las inicializa a cero.
+  digitalWrite(PWMA, LOW);            // Y ambas salidas se inicializa a cero.
   digitalWrite(PWMB, LOW);
   
   TCCR0B = TCCR0B & 0b11111000 | 0x1; // https://playground.arduino.cc/Code/PwmFrequency  &  https://playground.arduino.cc/Main/TimerPWMCheatsheet
   TCCR1B = TCCR1B & 0b11111000 | 0x1; // Aquí podemos variar la frecuencia del PWM con un número de 1 (32KHz) hasta 7 (32Hz). El número que pongamos es un divisor de frecuencia. Min.=7, Max.=1.
   
   attachInterrupt(digitalPinToInterrupt(encA), encoder, CHANGE); // En cualquier flanco ascendente o descendente
-  attachInterrupt(digitalPinToInterrupt(encB), encoder, CHANGE); // de los pines 2 y 3, actúa la interrupción.
+  attachInterrupt(digitalPinToInterrupt(encB), encoder, CHANGE); // de los pines 2 y 3 actúa la interrupción.
 
   // Límites máximo y mínimo; corresponde a Max.: 0=0V hasta 255=5V (PWMA), y Min.: 0=0V hasta -255=5V (PWMB). Ambos PWM se convertirán a la salida en valores absolutos, nunca negativos.
   outMax =  255.0;                    // Límite máximo del controlador PID.
   outMin = -outMax;                   // Límite mínimo del controlador PID.
   
   tmp = 25;
-  SetSampleTime(tmp);                 // Tiempo de muestreo.
+  SetSampleTime(tmp);                 // Se le asigna el tiempo de muestreo.
   
-  kp = 1.0;                           // Constantes PID iniciales. Los valores son los adecuados para un encoder de 334 ppr,
-  ki = 0.05;                          // pero como el lector de encoder está diseñado como x4 equivale a uno de 1336 ppr. (ppr = pulsos por revolución.)                  
+  kp = 1.0;                           // Constantes PID iniciales. Los valores son los adecuados para un encoder de 334 ppr (con un motor de 12v),
+  ki = 0.05;                          // pero como el lector de encoder está diseñado como x4, entonces equivale a uno de 1336 ppr. (ppr = pulsos por revolución.)                  
   kd = 25.0;
   
   SetTunings(kp, ki, kd);             // Llama a la función de sintonización y le envía los valores que hemos cargado anteriormente.
   
   Setpoint=(double)contador;          // Para evitar que haga cosas extrañas al ponerse en marcha o después de resetear, igualamos los dos valores para que comience estando quieto el motor.
   
-  imprimir(false);                    // Muestra los datos de sintonización y el tiempo de muestreo por el terminal serie.
+  imprimir(false);                    // Muestra los datos de sintonización, el tiempo de muestreo y la posición por el terminal serie.
 }
 
 void loop()
@@ -60,7 +63,7 @@ void loop()
   double Out = Compute();             // Llama a la función "Computer()" para calcular el error y darle una equivalencia de PWM y se carga en la variable 'Out'.
   
   // *********************************************** Control del Motor *************************************************
-  if (abs(error) == 0.0)                // Cuando está muy cerca del punto designado, parar el motor.
+  if (abs(error) == 0.0)              // Cuando está en el punto designado, parar el motor.
   { 
     digitalWrite(PWMA, 0);            // Pone a 0 los dos pines del puente en H.
     digitalWrite(PWMB, 0);
@@ -80,22 +83,22 @@ void loop()
     }
   }
   
-// Recepción de datos para mover el motor o modificar las constantes PID o el tiempo de muestreo.
+  // Recepción de datos para mover el motor o modificar las constantes PID o el tiempo de muestreo. Admite posiciones relativas y absolutas.
   if (Serial.available() > 0)         // Comprueba si ha recibido algún dato por el terminal serie.
   {
     cmd = Serial.read();              // "cmd" guarda el byte recibido.
-    if (cmd >  'Z') cmd -= 32;        // Si una letra entra minúscula la covierte en mayúscula.
-    if (cmd == 'W') Setpoint += 5.0;  // Si es la letra 'W' mueve 5 pasos hacia delante. Estos son movimientos relativos (por incrementos).
-    if (cmd == 'Q') Setpoint -= 5.0;  // Aquí son esos 5 pasos pero hacia atrás, si se pulsa la letra 'Q'.
-    if (cmd == 'S') Setpoint += 400.0;// Se repite lo mismo en el resto de las teclas que le hayamos dado alguna función.
+    if (cmd >  'Z') cmd -= 32;        // Si una letra entra en minúscula la covierte en mayúscula.
+    if (cmd == 'W') Setpoint += 5.0;  // Si (por ejemplo) es la letra 'W' mueve 5 pasos hacia delante. Estos son movimientos relativos.
+    if (cmd == 'Q') Setpoint -= 5.0;  // Aquí son esos 5 pasos pero hacia atrás si se pulsa la letra 'Q'.
+    if (cmd == 'S') Setpoint += 400.0;// Se repite lo mismo en el resto de las teclas.
     if (cmd == 'A') Setpoint -= 400.0;  
     if (cmd == 'X') Setpoint += 5000.0;
     if (cmd == 'Z') Setpoint -= 5000.0;
     if (cmd == '2') Setpoint += 12000.0;
     if (cmd == '1') Setpoint -= 12000.0;
     // Decodificador para modificar las constantes PID.
-    switch(cmd)                                                                        // Si ponemos en el terminal, por ejemplo "p2.5 i0.5 d40" y pulsas enter  tomará esos valores y los cargará en kp, ki y kd.
-    {                                                                                  // También se puede poner individualmente, por ejemplo "p5.5", sólo cambiará el parámetro kp, los mismo si son de dos en dos.
+    switch(cmd)                                                                            // Si ponemos en el terminal, por ejemplo "p2.5 i0.5 d40" y pulsas enter  tomará esos valores y los cargará en kp, ki y kd.
+    {                                                                                      // También se puede poner individualmente, por ejemplo "p5.5", sólo cambiará el parámetro kp, los mismo si son de dos en dos.
       case 'P': kp  = Serial.parseFloat(); SetTunings(kp, ki, kd); imprimir(false); break; // Carga las constantes y presenta en el terminal serie los valores de las variables que hayan sido modificadas.
       case 'I': ki  = Serial.parseFloat(); SetTunings(kp, ki, kd); imprimir(false); break;
       case 'D': kd  = Serial.parseFloat(); SetTunings(kp, ki, kd); imprimir(false); break;
@@ -104,11 +107,12 @@ void loop()
       case 'K': imprimir(false); break; 
       default:  imprimir(true);  break;
     }
-    digitalWrite(ledok, LOW);         // Cuando entra una nueva posición se apaga el led y no se volverá a encender hasta que el motor llegue a la posición que le hayamos designado.
+    digitalWrite(ledok, LOW);         // Cuando entra una posición nueva se apaga el led y no se volverá a encender hasta que el motor llegue a la posición que le hayamos designado.
   }
 }
 
-void encoder()                        // Encoder x4. Cuando se produzca cualquier cambio en el encoder esta parte hará que incremente o decremente el contador.
+// Encoder x4. Cuando se produzca cualquier cambio en el encoder esta parte hará que incremente o decremente el contador.
+void encoder()                        
 {
     ant=act;                          // Guardamos el valor 'act' en 'ant' para convertirlo en pasado.
     act=PIND & 12;                    // Guardamos en 'act' el valor que hay en ese instante en el encoder y hacemos un
@@ -124,6 +128,7 @@ void encoder()                        // Encoder x4. Cuando se produzca cualquie
     if(ant==12 && act==8)  contador--;
 }
 
+// Cálculo PID. 
 double Compute(void)
 {
    unsigned long now = millis();                  // Toma el número total de "ticks" que hay en ese instante. (1 tick = 1 milisengundo.)
@@ -132,22 +137,25 @@ double Compute(void)
    if(timeChange >= SampleTime)                   // Si se cumple el tiempo de muestreo entonces calcula la salida.
    {
      Input  = (double)contador;                   // Lectura del encoder óptico. El valor del contador se incrementa/decrementa a través de las interrupciones extrenas (pines 2 y 3).
-     error  = (Setpoint - Input)  * kp;           // Calcula el error proporcional.
+     
+     error  = (Setpoint - Input)  * kp;           // Calcula el error proporcional.   
      dInput = (Input - lastInput) * kd;           // Calcula el error derivativo.
-
-     if ((dInput == 0.0) || (abs(error) == 0.0)) ITerm += (error * ki); else ITerm -= (dInput / (kp * ki * kd)); // Evita sumar el error integral al proporcional cuando no le corresponde y suaviza la llegada al punto designado.
-     if (ITerm > outMax) ITerm = outMax; else if (ITerm < outMin) ITerm = outMin;  // Delimita el error integral para eliminar el "efecto windup".
      
-     double out = error + ITerm - dInput;          // Suma todos los errores, es la salida del control PID.
-     if (out > outMax) out = outMax; else if (out < outMin) out = outMin; // Delimita la salida para que pueda estar el PWM entre un valor de 0 y 5 voltios.
+     if ((dInput == 0.0) || (abs(error) == 0.0)) ITerm += (error * ki); else ITerm -= (dInput / (kp * ki * kd)); // Esta línea permite dos cosas. 1) Suaviza la llegada a la posición designada. 2) Hace que el error integral no sea tan dependiente del error proporcional, sólo actúa cuando está muy cerca de la posición designada, o cuando la velocidad del motor es constante o cuando es frenado totalmente por algún motivo.  
+     if (ITerm > outMax) ITerm = outMax; else if (ITerm < outMin) ITerm = outMin;                                // Delimita el error integral para eliminar el "efecto windup".
      
-     lastInput = Input;                            // Se guarda la posición para convertirla en pasado.
-     lastTime  = now;                              // Se guarda el tiempo   para convertirlo en pasado.
-     return out;                                   // Devuelve el valor de salida PID. Este 'out' es diferente del 'Out' del programa principal, sin embargo éste se transfiere al otro.
+     double Output = error + ITerm - dInput;      // Suma todos los errores, es la salida del control PID.
+     if (Output > outMax) Output = outMax; else if (Output < outMin) Output = outMin; // Delimita la salida para que pueda estar el PWM entre un valor de 0 y 5 voltios.
+     
+     lastInput = Input;                           // Se guarda la posición para convertirla en pasado.
+     lastTime  = now;                             // Se guarda el tiempo   para convertirlo en pasado.
+     
+     return Output;                               // Devuelve el valor de salida PID.
    }
 }
 
-void SetTunings(double kp, double ki, double kd)   // A las constantes KI y KD se le incluye el tiempo de muestreo. Así sólo se ha de hacer esto una sola vez.
+// Función de sintonización.
+void SetTunings(double kp, double ki, double kd)  // A las constantes KI y KD se les incluyen el tiempo de muestreo. Así sólo se ha de hacer esto una sola vez.
 {
    double SampleTimeInSec = ((double)SampleTime) / 1000.0;
    kp = kp;
@@ -155,11 +163,13 @@ void SetTunings(double kp, double ki, double kd)   // A las constantes KI y KD s
    kd = kd / SampleTimeInSec;
 }
 
-void SetSampleTime(int NewSampleTime)              // Función que se llama cuando queremos poner un tiempo de muestreo dado.
-{                                                  // Evita que el tiempo de muestreo sea 0 o negativo.
+// Función del tiempo de muestreo.
+void SetSampleTime(int NewSampleTime)             // Función para cuando queremos poner un tiempo de muestreo dado.
+{                                                 // Evita que el tiempo de muestreo sea 0 o negativo.
    if (NewSampleTime > 0) SampleTime = (unsigned long)NewSampleTime;
 }
 
+// Función para imprimir los datos en el terminal serie.
 void imprimir(boolean flag) // Si "flag" es 0, imprime en el terminal serie los datos de las contantes PID, tiempo de muestreo y posición. En los demás casos sólo imprime la posición del motor.
 {
   if (flag == false)
